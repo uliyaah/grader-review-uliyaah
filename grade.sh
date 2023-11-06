8b3413034cac79e43ff398dba1b9162509907d86
Uliyaah Dionisio <uliyaahd@gmail.com>
2023-11-06 11:49:38 -0800

    update

--- grade.sh
+++ grade.sh
@@ -8,6 +8,45 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+submission=`find student-submission -name "*.java"`
+
+if [[ $submission == "student-submission/ListExamples.java" ]]
+    then 
+        cp -r $submission grading-area
+    else
+        echo "Incorrect file submitted"
+        exit
+fi
+
+echo $submission
+
+cp TestListExamples.java grading-area
+
+cd grading-area
+
+javac ListExamples.java TestListExamples.java > error.txt
+cat error.txt
+errorCode=`cat error.txt`
+
+
+echo $errorCode
+
+
+if [[ $errorCode == "" ]]
+    then 
+        echo "Finished Compiling" 
+        javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+        java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+    else
+        echo "ERROR: Did not compile!"
+        exit
+fi
+
+
+
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
